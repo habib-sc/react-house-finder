bd2f31a6749ff716bb57c9d2f3310b6fd910c9b0
habib-sc <habib97.cs@gmail.com>
2022-04-03 00:01:41 +0600

    search category design change

--- src/Home/CategorySearch/CategorySearch.js
+++ src/Home/CategorySearch/CategorySearch.js
@@ -11,13 +11,13 @@ const CategorySearch = () => {
     return (
         <div className='w-full'>
             <div className='flex justify-center'>
-                <button onClick={ () => {getButtonCategory('all')}} className={`px-3 py-2 rounded-t-lg mx-2 ${searchCategory === 'all' ? "bg-emerald-500 text-white" : "bg-white"}`}>All</button>
-                <button onClick={ () => {getButtonCategory('for-rent')}} className={`px-3 py-2 rounded-t-lg mx-2 ${searchCategory === 'for-rent' ? "bg-emerald-500 text-white" : "bg-white"}`}>For Rent</button>
-                <button onClick={ () => {getButtonCategory('for-sale')}} className={`px-3 py-2 rounded-t-lg mx-2 ${searchCategory === 'for-sale' ? "bg-emerald-500 text-white" : "bg-white"}`}>For Sale</button>
+                <button onClick={ () => {getButtonCategory('all')}} className={`px-3 py-2 rounded-t mx-1 ${searchCategory === 'all' ? "bg-emerald-500 text-white" : "bg-white"}`}>All</button>
+                <button onClick={ () => {getButtonCategory('for-rent')}} className={`px-3 py-2 rounded-t mx-1 ${searchCategory === 'for-rent' ? "bg-emerald-500 text-white" : "bg-white"}`}>For Rent</button>
+                <button onClick={ () => {getButtonCategory('for-sale')}} className={`px-3 py-2 rounded-t mx-1 ${searchCategory === 'for-sale' ? "bg-emerald-500 text-white" : "bg-white"}`}>For Sale</button>
             </div>
             <div className='flex justify-center'>
-                <div className='py-2 px-3 rounded-lg bg-emerald-500 w-[600px] flex justify-center items-center'>
-                    <input type="text" placeholder='Search your house' className='w-96 px-2 py-2 rounded-lg mr-3' />
+                <div className='py-1 px-3 rounded bg-emerald-500 w-[600px] flex justify-center items-center'>
+                    <input type="text" placeholder='Search your house' className='w-96 px-2 py-1 rounded mr-3' />
                     <button>Search</button>
                 </div>
             </div>
